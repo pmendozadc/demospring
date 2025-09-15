641e943a3cd6cdaadf0da958d619db37a76afc73
Paul Mendoza del Carpio <p.mendozadc@ulasalle.edu.pe>
2025-09-15 16:56:55 -0500

    Inicio

---
+++ src/main/java/pe/edu/uni/demospring/DemospringApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.uni.demospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemospringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemospringApplication.class, args);
+	}
+
+}
